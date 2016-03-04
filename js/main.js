5f2cc225772f3659bdc6760fd603b841fc1aaa74
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-03-04 03:04:39 -0500

    added var j for counting compare

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ var letter = prompt('Enter a letter');
 
 var i;
 
+var j;
+
 for (i = 0; i < 10; i++) {
-		for (j = 0; j < i; j++) {
+	for (j = 0; j < i; j++) {
   document.write(letter);
   }
   document.write(letter + '<br>');
